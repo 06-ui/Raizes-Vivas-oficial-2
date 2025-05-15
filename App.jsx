8e646d6cc1c3823b47e0aadb6980c3e1173221da
06-ui <ines.rbaptistafernandes@gmail.com>
2025-05-15 22:14:40 +0100

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-export default function RaizesVivasSite() {
-  return (
-    <div className='text-center p-10'>
-      <h1 className='text-4xl font-bold text-green-700'>Raízes Vivas</h1>
-      <p className='mt-4 text-xl'>Raízes que acolhem, tradições que inspiram.</p>
-      <p className='mt-6'>Este site foi criado com React, Vite e Tailwind CSS.</p>
-    </div>
-  );
-}
